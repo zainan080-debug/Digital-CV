9d67bc05b4ddc7b0ffb806b5b6299bc63f097dcd
zainan080-debug <zaina.n080@gmail.com>
2025-12-05 23:25:43 +0500

    Create About.js

---
+++ src/components/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section id="about">
+      <h2>About Me</h2>
+      <p>
+        “Hi! I’m Zaina, a passionate Full-Stack Developer specializing in React JS. I love transforming ideas into clean, responsive, and interactive web applications. With a strong eye for detail and a knack for problem-solving, I enjoy crafting seamless user experiences and exploring modern web technologies. Always curious and eager to learn, I thrive on turning challenges into creative solutions.”
+      </p>
+    </section>
+  );
+};
+
+export default About;
